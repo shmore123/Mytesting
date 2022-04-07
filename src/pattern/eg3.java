8941e004ea00cc4232c00df8107449c952216731
Shubham <shmorem33@gmail.com>
2022-04-07 19:37:22 +0530

    commit first

---
+++ src/pattern/eg3.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class eg3 {
+
+	public static void main(String[] args) {
+		int Star=5;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=Star;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			Star--;
+		}
+
+	}
+
+}
